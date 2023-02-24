d9e6f394fa1a3b93893521f3c24de3dedd3e975b
Christian <christianday954@ymail.com>
2023-02-24 14:38:48 -0800

    Finish bettersubsum

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/practice.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/practice.rb
@@ -38,46 +38,17 @@ p my_min(list)  # =>  -5
 
 ## Largest contiguous sub-sum
 
-You have an array of integers and you want to find the largest contiguous
-(together in sequence) sub-sum. Find the sums of all contiguous sub-arrays and
-return the max.
+# You have an array of integers and you want to find the largest contiguous
+# (together in sequence) sub-sum. Find the sums of all contiguous sub-arrays and
+# return the max.
 
-Example:
+# ### Phase I
 
-```ruby
-list = [5, 3, -7]
-largest_contiguous_subsum(list) # => 8
+# Write a function that iterates through the array and finds all sub-arrays using
+# nested loops. First make an array to hold all sub-arrays. Then find the sums of
+# each sub-array and return the max.
 
-# possible sub-sums
-[5]           # => 5
-[5, 3]        # => 8 --> we want this one
-[5, 3, -7]    # => 1
-[3]           # => 3
-[3, -7]       # => -4
-[-7]          # => -7
-```
-
-Example 2:
-
-```ruby
-list = [2, 3, -6, 7, -6, 7]
-largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
-```
-
-Example 3:
-
-```ruby
-list = [-5, -1, -3]
-largest_contiguous_subsum(list) # => -1 (from [-1])
-```
-
-### Phase I
-
-Write a function that iterates through the array and finds all sub-arrays using
-nested loops. First make an array to hold all sub-arrays. Then find the sums of
-each sub-array and return the max.
-
-Discuss the time complexity of this solution.
+# Discuss the time complexity of this solution.
 
 def subsum(list)
     subs = []
@@ -96,24 +67,24 @@ end
 
 ### Phase II
 
-Let's make a better version. Write a new function using O(n) time with O(1)
-memory. Keep a running tally of the largest sum. To accomplish this efficient
-space complexity, consider using two variables. One variable should track the
-largest sum so far and another to track the current sum. The rest is left to
-you.
+# Let's make a better version. Write a new function using O(n) time with O(1)
+# memory. Keep a running tally of the largest sum. To accomplish this efficient
+# space complexity, consider using two variables. One variable should track the
+# largest sum so far and another to track the current sum. The rest is left to
+# you.
 
-Get your story straight, and then explain your solution's time complexity to
-your Instructor.
+# Get your story straight, and then explain your solution's time complexity to
+# your Instructor.
 
 
-def bettersubsum(list) [2,3,-6,7,-6,7]
+def bettersubsum(list) 
     max = list[0]
     current_max = 0
     (0...list.length).each do |i| #going through the list
-        if list[i] > current_max + list[i] 2 > 2 +2
+        if list[i] > current_max + list[i] 
             current_max = list[i]
         else 
-            current_max = current_max + list[i] 2 +2 = 4 
+            current_max = current_max + list[i]
         end
     
         max = current_max if current_max > max
@@ -122,30 +93,9 @@ def bettersubsum(list) [2,3,-6,7,-6,7]
     return max
 end
 
-def bettersubsum(list) [2,3,-6,7,-6,7]
-    max = list[0]
-    current_max = list[0]
-    (0...list.length).each do |i| 
-        max = max + list[i]
-        if current_max < max
-            current_max = max
 
-        else 
-            current_max = current_max + list[i] 
-        end
-    
-        max = current_max if current_max > max
-    end
-
-    return max
-end
-
-def bettersubsum(list)
-    global_max = list[0]
-    current_max = 0
-    (0...list.length).each do |i|
-        current_max = c
-
-
+list = [2, 3, -6, 7, -6, 7]
+p list
+p bettersubsum(list)
 
  
