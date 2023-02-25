cc1d8f26158d1aa2a9e0b4c0ab718976e1aa6b5a
Christian <christianday954@ymail.com>
2023-02-24 16:28:49 -0800

    Add two_sum

--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum?.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum?.rb
@@ -9,43 +9,11 @@ def bad_two_sum?(arr, target_sum)
     end
 
     return false
-                
 end
 
+# Write a second solution called `okay_two_sum?` that uses sorting. Make sure it
+# works correctly.
 
-Given an array of unique integers and a target sum, determine whether any two
-integers in the array sum to that amount.
-
-ruby
-def two_sum?(arr, target_sum)
-    # your code here...
-end
-
-arr = [0, 1, 5, 7]
-two_sum?(arr, 6) # => should be true
-two_sum?(arr, 10) # => should be false
-
-If you weren't worried about time complexity, what would be the most
-straightforward way to solve this problem? To start with, you could check every
-possible pair. If you sum each element with every other, you're sure to either
-find the pair that sums to the target or determine that no such pair exists.
-
-This is the **brute-force** solution. It's essentially hitting the problem with
-a sledge hammer. There are faster and more elegant ways to solve the problem,
-but you know for sure that this will get the job done.
-
-Let's start by implementing the brute force solution. Write a method called
-`bad_two_sum?` that checks every possible pair.
-
-Make sure that you don't pair an element with itself. However, you don't need to
-bother checking for summing the same pair twice; that won't affect your result.
-
-(**Note:** You can cut the running-time significantly by returning early and by
-not checking pairs more than once. However, these micro-optimizations will not
-improve the time complexity of the solution. Do you see why?)
-
-Once you're done, write a comment with your solution's time complexity.
-
-
-
-
+# **Hint:** There are a couple ways to solve this problem once the array is
+# sorted. One way involves using a very cheap algorithm that can only be used on
+# sorted data sets. What are some such algorithms you know?
